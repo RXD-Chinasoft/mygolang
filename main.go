d3d31d82b07f205a1ea7b8b73042f24f3ec22d76
huangsonglu <jason.huang@chinasofti.com>
2018-04-16 12:14:54 +0800

    add log

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
 )
 
 func main() {
+	log.Print("Starting the service...")
 	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
 			fmt.Fprint(w, "Hello! Your request was processed.")
 	})
-	http.ListenAndServe(":8000", nil)
-	fmt.Println("hello go...")
+	log.Print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
