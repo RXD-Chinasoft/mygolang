ef0bd74a0ce6c6fc948ab97cf53c3f6f7d54cd59
huangsonglu <jason.huang@chinasofti.com>
2018-04-16 14:27:37 +0800

    add test and port

--- handlers/home.go
+++ handlers/home.go
@@ -7,5 +7,5 @@ import (
 
 // home is a simple HTTP handler function which writes a response.
 func home(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "Hello! Your request was processed..")
+	fmt.Fprint(w, "Hello! Your request was processed.")
 } 
--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 	"log"
 	"github.com/RXD-Chinasoft/mygolang/handlers"
+	"os"
 )
 
 func main() {
 	log.Print("Starting the service....")
+	port := os.Getenv("PORT")
 	router := handlers.Router()
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
