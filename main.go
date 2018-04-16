f9a73510ef374816b48a604c563d852a52ccf683
huangsonglu <jason.huang@chinasofti.com>
2018-04-16 12:08:12 +0800

    add http port

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/home", func(w http.ResponseWriter, _ *http.Request) {
+			fmt.Fprint(w, "Hello! Your request was processed.")
+	})
+	http.ListenAndServe(":8000", nil)
 	fmt.Println("hello go...")
 }
